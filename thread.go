8e02c0c1f5eb226ca4aa7b14b8075bd8aebfd5a8
Lee Archer <lbn@users.noreply.github.com>
2016-11-05 17:32:13 +0000

    Implement thread view

--- thread.go
+++ thread.go
@@ -2,11 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/aymerick/raymond"
+	"github.com/gorilla/mux"
 )
 
+var (
+	threadTemplate *raymond.Template
+)
+
+func init() {
+	threadTemplate, _ = raymond.ParseFile("views/thread.html")
+}
+
 func handleThread(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	var posts []Post
+
+	strID := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(strID)
+
+	err := db.Select(&posts, "SELECT * FROM post WHERE thread_id = $1", id)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	v := threadTemplate.MustExec(map[string]interface{}{
+		"id":    posts[0].ThreadID,
+		"posts": posts,
+	})
+	fmt.Fprintf(w, v)
 }
 
 func handleThreadPost(w http.ResponseWriter, r *http.Request) {
